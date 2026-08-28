cbb9b48fee2992762531ec6a22ec783d25490cb1
kopperlabhavyasri-6 <vtu29174@veltech.edu.in>
2026-08-28 15:26:49 +0530

    Time: 0 ms (100%), Space: 46.2 MB (74.59%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -1,53 +1,27 @@
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode current = head;
+        int count = 0;
 
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-
-        ListNode groupPrev = dummy;
-
-        while (true) {
-
-            // Find the kth node
-            ListNode kth = getKthNode(groupPrev, k);
-
-            // Less than k nodes remain
-            if (kth == null) {
-                break;
-            }
-
-            // Node after the group
-            ListNode groupNext = kth.next;
-
-            // Reverse the group
-            ListNode prev = groupNext;
-            ListNode curr = groupPrev.next;
-
-            while (curr != groupNext) {
-                ListNode next = curr.next;
-                curr.next = prev;
-                prev = curr;
-                curr = next;
-            }
-
-            // Connect previous part to reversed group
-            ListNode oldGroupStart = groupPrev.next;
-            groupPrev.next = kth;
-
-            // Move groupPrev to the end of reversed group
-            groupPrev = oldGroupStart;
+        while (current != null && count < k) {
+            current = current.next;
+            count++;
         }
 
-        return dummy.next;
-    }
+        if (count < k)
+            return head;
 
-    private ListNode getKthNode(ListNode curr, int k) {
+        ListNode previous = null;
+        current = head;
 
-        while (curr != null && k > 0) {
-            curr = curr.next;
-            k--;
+        for (int i = 0; i < k; i++) {
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
         }
 
-        return curr;
+        head.next = reverseKGroup(current, k);
+        return previous;
     }
 }
