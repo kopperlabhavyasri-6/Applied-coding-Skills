829acbd5a08250865272faee35f0f10a4c62b1ab
kopperlabhavyasri-6 <vtu29174@veltech.edu.in>
2026-08-28 09:58:05 +0530

    Time: 3 ms (17.97%), Space: 47.8 MB (38.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int index = 0;
+        int snowBallSize = 0;
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]==0){
+                snowBallSize++;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[index] = nums[i];
-                index++;
             }
-        }
-
-        while (index < nums.length) {
-            nums[index] = 0;
-            index++;
+            else if (snowBallSize >0){
+                int t =nums [i];
+                nums [i]=0;
+                nums[i-snowBallSize]=t;
+            }
         }
     }
 }
